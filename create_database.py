69095dafb50c31f921969a2ccfe1e4a71a59cd4e
Maharshi <maharshilavana@gmail.com>
2026-08-19 20:56:04 +0530

    Learned about vector database and created vector store for system_design pdf using recursive chunking

---
+++ create_database.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+load_dotenv()
+
+data = PyPDFLoader("./system_design.pdf")
+docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs)
+
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+vector_store = Chroma.from_documents(
+    documents = chunks,
+    embedding = embedding_model,
+    persist_directory="chrom_db"
+)
--- main.py
+++ main.py
@@ -1,27 +1,5 @@
 from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain_mistralai import ChatMistralAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-load_dotenv()
-
-data = TextLoader("./document-loaders/haaland.txt")
-docs = data.load()
-
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000,
-    chunk_overlap = 200
-)
-chunks = splitter.split_documents(docs)
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system","you are an AI that is a football enthusiast and summarizes the text"),
-        ("human","{data}")
-    ]
-)
-final_prompt = prompt.format_messages(data = docs[0].page_content)
-
-model = ChatMistralAI(model="mistral-small-2506")
-response = model.invoke(final_prompt)
-print(response.content)
+from langchain
